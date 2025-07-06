28ced09b4ca205862af17c25d2558096440f2142
Tory <signalbeachhouse@gmail.com>
2025-07-06 14:22:20 -0700

    editwhisper

--- src/WhisperPage.jsx
+++ src/WhisperPage.jsx
@@ -1,7 +1,12 @@
-// src/WhisperPage.jsx
 import React from 'react';
 import ChatWindow from './components/ChatWindow';
 
 export default function WhisperPage() {
-  return <ChatWindow userName="Low Tide" />;
+  return (
+    <div>
+      <h1 style={{ color: 'teal', textAlign: 'center' }}>🜂 Whisper Mode 🜂</h1>
+      <ChatWindow userName="Low Tide" />
+    </div>
+  );
 }
+

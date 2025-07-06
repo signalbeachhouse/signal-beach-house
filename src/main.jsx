05072b58f4b3ab449d13b015c387da06be968e2e
Tory <signalbeachhouse@gmail.com>
2025-07-06 16:13:14 -0700

    routing logic update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './components/App.jsx';
+import WhisperPage from './components/WhisperPage.jsx';
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/whisper" element={<WhisperPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
 
 

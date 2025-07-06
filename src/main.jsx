99c17a9a48fa144bb0640b7dbe631c2dcd318590
Tory <signalbeachhouse@gmail.com>
2025-07-06 02:09:48 -0700

    chat work

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
-console.log("📦 main.jsx loaded");
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App.jsx';
+import WhisperPage from './WhisperPage.jsx';
 import './styles.css';
+
+console.log("🌊 main.jsx loaded");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/whisper" element={<WhisperPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+

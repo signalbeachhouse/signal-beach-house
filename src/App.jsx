6d5a90ccae565883d656f0e9d3f509dc7fba0112
Tory <signalbeachhouse@gmail.com>
2025-07-06 02:15:21 -0700

    redundancy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChatWindow from './components/ChatWindow';
-import WhisperPage from './WhisperPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ChatWindow userName="Low Tide" />} />
-        <Route path="/whisper" element={<WhisperPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ChatWindow userName="Low Tide" />
   );
 }
 
 export default App;
-console.log("Triggering redeploy with real change");
+
 
 
 

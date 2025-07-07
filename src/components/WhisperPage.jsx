cebf6d357c859953170a515f24212595c76fef48
Tory <signalbeachhouse@gmail.com>
2025-07-07 15:47:37 -0700

    twind update

--- src/components/WhisperPage.jsx
+++ src/components/WhisperPage.jsx
@@ -4,7 +4,7 @@ import ChatWindow from './ChatWindow';
 
 export default function WhisperPage() {
   return (
-    <div className="min-h-screen bg-[#f7f1eb] flex items-center justify-center">
+    <div className="min-h-screen bg-[#f7f1eb] flex items-center justify-center px-4">
       <ChatWindow />
     </div>
   );

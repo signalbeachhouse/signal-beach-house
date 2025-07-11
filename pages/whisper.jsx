54b114a9cc2b9ed8bf51d276bba491400997a2f4
Tory <signalbeachhouse@gmail.com>
2025-07-11 00:45:11 -0700

    nowhite box

--- pages/whisper.jsx
+++ pages/whisper.jsx
@@ -3,10 +3,9 @@ import ChatWindow from "../src/components/ChatWindow";
 
 export default function WhisperPage() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center
+">
       <ChatWindow />
     </div>
   );
 }
-
-
